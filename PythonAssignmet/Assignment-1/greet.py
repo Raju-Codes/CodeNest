5d412fe949bc92833322058da57bc728cf7ab951
Raju-Codes <rajub.codes@gmail.com>
2025-07-21 11:30:56 +0530

    Update greet.py

--- PythonAssignmet/Assignment-1/greet.py
+++ PythonAssignmet/Assignment-1/greet.py
@@ -1,1 +1,6 @@
-
+## input to take first name
+fName = input("Enter your first name: ")
+## input to take Last name
+lName = input("Enter your last name: ")
+## printing greeting note
+print("Hello, "+ fName +" "+ lName +"! welcome to the python program")
