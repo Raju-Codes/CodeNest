63e517a1eb2212ef9c733183a129bbc1641b457f
Raju-Codes <rajub.codes@gmail.com>
2025-07-22 11:56:07 +0530

    added

---
+++ PythonAssignmet/Assignment2/EvenOdd.py
@@ -0,0 +1,7 @@
+## to check whether num from input is even or odd
+inputs = int(input("Enter a number: "))
+
+if(inputs%2==0):
+    print(inputs,"is an even number.")
+else:
+    print(inputs,"is an odd number.")
---
+++ PythonAssignmet/Assignment2/SumofInteger.py
@@ -0,0 +1,6 @@
+## add variable is used to store addition number in final result;
+add = 0
+for i in range(1,51):
+    add=add+i
+print("The sum of numbers from 1 to 50 is:", add)
+
